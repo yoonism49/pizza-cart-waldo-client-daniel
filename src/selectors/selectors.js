462163e297667d544d49216d72bde7eeb2e0fc21
yoonism49 <yoonism49@gmail.com>
2018-02-12 15:39:43 -0800

    Delete selectors.js

--- src/selectors/selectors.js
+++
@@ -1,9 +0,0 @@
-import { createSelector } from 'reselect';
-
-export const getPizzas = state => state.get('pizzas');
-
-export const getPizzaList = createSelector(
-  getPizzas,
-  pizzas => pizzas.get('pizzaSizes')
-);
-
